7b7e5893a7b9923800c0b698b9966422e5fa367b
YOUR_USERNAME <muhamamdhamzaleo@gmail.com>
2017-10-26 20:13:03 +0500

    Updated NaiveBayes

--- Naive Bayes/NaiveBayes.py
+++ Naive Bayes/NaiveBayes.py
@@ -1,6 +1,8 @@
 import numpy as np
+
 X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
 Y = np.array([1, 1, 1, 2, 2, 2])
+
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
 clf.fit(X, Y)
